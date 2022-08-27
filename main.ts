13b858ea3efe18d1d635ccf703ca47a8b2f3009e
Tak Tran <contact@tutaktran.com>
2022-08-27 22:04:12 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+})
+music.setVolume(51)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # . # . #
+    # . . . #
+    # # # # #
+    `)
+basic.forever(function () {
+	
+})
